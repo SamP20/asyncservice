adb81386379b837a90a3a45423394ab84d1bb69b
Samuel Partridge <partridgeSL@cardiff.ac.uk>
2018-10-30 16:13:00 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="samp20-asyncservice",
-    version="1.1.0",
+    version="1.1.1",
     author="Sam Partridge",
     description="Base to build microservices upon",
     long_description=long_description,

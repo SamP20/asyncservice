41e2d2553fc59e8adfab1ebd57f49800c34b2b0f
SamP20 <samp20@users.noreply.github.com>
2018-11-06 11:56:31 +0000

    Bump version

--- README.md
+++ README.md
@@ -6,6 +6,10 @@ Base to build microservices upon.
 Version history
 ---------------
 
+### 1.2.0
+
+Change HttpServer to accept keyword args instead of a dict
+
 ### 1.0.0
 
 * Initial release
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="samp20-asyncservice",
-    version="1.1.1",
+    version="1.2.0",
     author="Sam Partridge",
     description="Base to build microservices upon",
     long_description=long_description,
